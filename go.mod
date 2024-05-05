6eb3888260f3a91b9535cae055f65f9d3cf861a3
Vinuka Kodituwakku <vinuka.t@pm.me>
2024-05-05 19:49:42 +0530

    Added the ability to stream from a kafka topic

---
+++ go.mod
@@ -0,0 +1,19 @@
+module github.com/VinukaThejana/kafka-stream
+
+go 1.22.1
+
+require (
+	github.com/go-chi/chi/v5 v5.0.12
+	github.com/go-chi/cors v1.2.1
+	github.com/google/uuid v1.6.0
+	github.com/segmentio/kafka-go v0.4.47
+)
+
+require (
+	github.com/klauspost/compress v1.17.8 // indirect
+	github.com/pierrec/lz4/v4 v4.1.21 // indirect
+	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
+	github.com/xdg-go/scram v1.1.2 // indirect
+	github.com/xdg-go/stringprep v1.0.4 // indirect
+	golang.org/x/text v0.13.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,94 @@
+// Streaming
+package main
+
+import (
+	"context"
+	"crypto/tls"
+	"fmt"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
+	"github.com/google/uuid"
+	"github.com/segmentio/kafka-go"
+	"github.com/segmentio/kafka-go/sasl/scram"
+)
+
+var (
+	username string
+	password string
+	broker   string
+)
+
+func init() {
+	username = os.Getenv("KAFKA_USERNAME")
+	password = os.Getenv("KAFKA_PASSWORD")
+	broker = os.Getenv("KAFKA_BROKER")
+
+	if username == "" || password == "" || broker == "" {
+		fmt.Println("username, password or the broker is not present in the enviroment")
+		os.Exit(0)
+	}
+}
+
+func main() {
+	router := chi.NewRouter()
+
+	router.Use(middleware.RequestID)
+	router.Use(middleware.RealIP)
+	router.Use(middleware.Logger)
+	router.Use(middleware.Recoverer)
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		AllowCredentials: true,
+	}))
+
+	router.Get("/{topic}", func(w http.ResponseWriter, r *http.Request) {
+		topic := chi.URLParam(r, "topic")
+		if topic == "" {
+			w.WriteHeader(http.StatusBadRequest)
+			fmt.Fprint(w, "topic is not provided")
+			return
+		}
+
+		w.Header().Set("Content-Type", "text/event-stream")
+		w.Header().Set("Cache-Control", "no-cache")
+		w.Header().Set("Connection", "Keep-alive")
+
+		flusher, ok := w.(http.Flusher)
+		if !ok {
+			http.Error(w, "Streaming not supported", http.StatusInternalServerError)
+			return
+		}
+
+		mechanism, _ := scram.Mechanism(scram.SHA512, username, password)
+		reader := kafka.NewReader(kafka.ReaderConfig{
+			Brokers:     []string{broker},
+			GroupID:     uuid.New().String(),
+			Topic:       topic,
+			StartOffset: kafka.LastOffset,
+			Dialer: &kafka.Dialer{
+				SASLMechanism: mechanism,
+				TLS:           &tls.Config{},
+			},
+		})
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second*3600)
+		defer cancel()
+
+		for {
+			message, _ := reader.ReadMessage(ctx)
+			fmt.Fprintf(w, "data: %s\n\n", string(message.Value))
+			flusher.Flush()
+		}
+	})
+
+	err := http.ListenAndServe(":8080", router)
+	if err != nil {
+		panic(err)
+	}
+}
